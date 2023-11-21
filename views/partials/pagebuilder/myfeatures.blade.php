5b9969155ed61e7d7822ed061fe74fc5e118523d
Sasha Beep <thecomicz@gmail.com>
2023-11-21 15:16:01 +0300

    Update myfeatures.blade.php

--- views/partials/pagebuilder/myfeatures.blade.php
+++ views/partials/pagebuilder/myfeatures.blade.php
@@ -1,6 +1,6 @@
 @if(!empty($item['features']))
 <section class="{{ $item['padding'] }}">
-	@if(!empty($item['title']))<div class="row col text-center mb-4"><h2>{{$item['title']}}</h2></div>@endif
+	@if(!empty($item['title']))<div class="row col text-center mb-4"><h2>{{ $item['title'] }}</h2></div>@endif
 	<div class="row">
 		<div class="col-12 col-lg-10 offset-lg-1">
 			<div class="row xs-up-1 md-up-2 lg-up-3 cbfix">
